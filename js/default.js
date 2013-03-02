05aef6ceea00217f272f4011d831496570710ef4
Alexsander Akers <a2@pandamonia.us>
2013-03-02 12:00:38 -0500

    Whee!
    Signed-off-by: Alexsander Akers <a2@pandamonia.us>

--- js/default.js
+++ js/default.js
@@ -1,22 +1,16 @@
 ;(function ($, window, undefined) {
-	$.getJSON('http://data.adicu.com/athletics?win=true&pretty=true&api_token=51314d99a237900002959a87&jsonp=?', function(data) {
+	$.getJSON('http://data.adicu.com/athletics?game_after=1970-01-01%2000%3A00%3A00&api_token=51314d99a237900002959a87&jsonp=?', function(data) {
 		$.each(data.data, function(i, e) {
 			console.log(data);
-			$('#st')
-			.delay(100)
-			.animate({'opacity' : 0}, 100)
-			.queue(function(n) {
-				$(this).text(e.title);
-				if( e.win ) {
-				$(this).text($(this).text() + ' YES');
-				}
-				else {
-				$(this).text($(this).text() + ' NO');
-				}
-			       	n();
-			})
-			.animate({'opacity' : 1}, 100)
-			;
+			$('#game, h2')
+				.animate({'opacity': 0}, 500)
+				.queue(function(n) {
+					$('#game').text(e.title);
+					$('h2').text(e.win == null ? "IDK!" : e.win ? "YES!" : "NO!");
+					n();
+				})
+				.animate({'opacity': 1}, 500)
+				.delay(2000);
 		});
 	});
 })(jQuery, this);
