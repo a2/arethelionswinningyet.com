9068942420320fc441894a06c3152654f38815c8
Alexsander Akers <a2@pandamonia.us>
2013-03-02 12:43:33 -0500

    Show "TBD!" for future games; use Futura if Impact is unavailable
    Signed-off-by: Alexsander Akers <a2@pandamonia.us>

--- js/default.js
+++ js/default.js
@@ -1,11 +1,24 @@
 ;(function ($, window, undefined) {
 	$.getJSON('http://data.adicu.com/athletics?game_after=1970-01-01%2000%3A00%3A00&api_token=51314d99a237900002959a87&jsonp=?', function(data) {
-		$.each(data.data, function(i, e) {
+		var now = new Date();
+		$.each((function(v) {
+		    for(var j, x, i = v.length; i; j = parseInt(Math.random() * i), x = v[--i], v[i] = v[j], v[j] = x);
+		    return v;
+		})(data.data), function(i, e) {
 			$('#game, h2')
 				.animate({'opacity': 0}, 500)
 				.queue(function(n) {
 					$('#game').text(e.title);
-					$('h2').text(e.win == null ? "IDK!" : e.win ? "YES!" : "NO!");
+					$('h2').text(function() {
+						if (new Date(e.time) > now)
+							return "TBD!";
+						else if (e.win == null)
+							return "IDK!";
+						else if (e.win)
+							return "YES!";
+						else
+							return "NO!";
+					});
 					n();
 				})
 				.animate({'opacity': 1}, 500)
